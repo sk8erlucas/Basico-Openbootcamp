cb71712d241313f6bf2db791636e723cb445eca2
Lucas Laino <sk8erlucas@hotmail.com>
2022-09-10 21:48:07 -0300

    modified:   ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -2,4 +2,20 @@ def suma(a, b):
     return a + b
 
 resultado = suma(1, 2)
-print(resultado)
+print(f'La suma de 1 y 2 es {resultado}')
+
+
+class coche():
+    puertas = 0
+
+    def AumentarPuertas(self):
+        self.puertas += 1
+
+miCoche = coche()
+
+miCoche.AumentarPuertas()
+print(f'El coche tiene {miCoche.puertas} puerta')
+
+
+
+    
