e583a11fb4744a1bed8959206171d5014bf2b84f
Lucas Laino <sk8erlucas@hotmail.com>
2022-09-13 10:11:06 -0300

    Changes to be committed: 	modified:   practicas.py

--- practicas.py
+++ practicas.py
@@ -1,6 +1,18 @@
 
-temp = 1 + 2
+class auto:
+    _puertas = 0
 
-print(tecmp)
+    def __init__(self, cantPuertas):
+        if cantPuertas > 5:
+            self._puertas = 4
+        else:
+            self._puertas = cantPuertas
 
-print("Qui no debe llegar")
+    def getPuertas(self):
+        return self._puertas
+
+auto1 = auto(5)
+auto2 = auto(10)
+
+print(auto1.getPuertas())
+print(auto2.getPuertas())
