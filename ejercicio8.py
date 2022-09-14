94ca36e1dc7fadc3d03a420a6af0225edad3cae6
Lucas Laino <sk8erlucas@hotmail.com>
2022-09-14 08:19:55 -0300

    Changes to be committed: 	modified:   ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -9,8 +9,23 @@ class persona:
     def getEdad(self):
         return self._edad
 
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    def getNombre(self):
+        return self._nombre
+
+    def setTelefono(self, telefono):
+        self._telefono = telefono
+    def getTelefono(self):
+        return self._telefono
+
 persona1 = persona()
 
 persona1.setEdad(36)
-print(persona1.getEdad())
+print("La edad es", persona1.getEdad())
 
+persona1.setNombre("Lucas")
+print("El nombre es", persona1.getNombre())
+
+persona1.setTelefono("1111-1111")
+print("El telefono es", persona1.getTelefono())
